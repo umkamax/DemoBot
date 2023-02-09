007afa2fb043d9e87aa41c9c2e72559d071d1548
maksim.shilin <maksim.shilin@nvi-solutions.com>
2023-02-09 16:55:33 +0300

    Initial commit

---
+++ src/test/java/com/example/umkamax/demobot/DemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umkamax.demobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
